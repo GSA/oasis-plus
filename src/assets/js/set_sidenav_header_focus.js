37205069472f6b4251b729c97cd671c7a85783d5
weiwang-gsa <wei.wang@gsa.gov>
2024-02-21 10:59:20 -0500

    added customized JS for handling side nav focus on headers

---
+++ src/assets/js/set_sidenav_header_focus.js
@@ -0,0 +1,19 @@
+// JS function that is used to set focus on the side nav sub menus that leads to h2
+export function setHeaderFocus(){
+   
+    // get all the side nav links that leads to h2 headers
+    const sidenav_links = document.querySelectorAll('#usa-sidenav__sublist_header a')
+    // Add click event listener to each sub menu link
+    sidenav_links.forEach(link => {
+        link.addEventListener('click', (event) => {
+        // clear all
+        sidenav_links.forEach(item => {
+            item.classList.remove('usa-current');
+        });
+        // set the usa-current class to the clicked sub menu item link
+        link.classList.add('usa-current');
+        })
+    });
+    
+}
+ 

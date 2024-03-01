694fcfe4c7a90ffef88c717915ede015c46a7afc
Mark Meyer <mark.meyer@gsa.gov>
2024-03-01 10:45:56 -0900

    add sitemap, which will be needed for search.gov.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,28 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
 import { defineConfig } from 'astro/config';
 import process_anchors from "./src/plugins/process_anchors";
 import process_image_urls from './src/plugins/process_image_urls';
 import table_row_headers from './src/plugins/table_row_headers';
 
-import mdx from "@astrojs/mdx";
+const BASE_PATH = "/oasis-plus"
+
+function serialize(item) {
+  /* Rewrite urls for sitemap to put them under a subdirectory */
+  const url = new URL(item.url);
+  url.pathname = `${BASE_PATH}${url.pathname}`
+  item.url = url.toString();
+  return item;
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap({
+    serialize
+})],
   outDir: '_site',
+  site: 'https://www.gsa.gov/',
   base: process.env.BASEURL,
   trailingSlash: 'always',
   markdown: {

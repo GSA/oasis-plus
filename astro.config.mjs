d0ec4139fc9ad0f76866217aa23d9e462ad125ce
Mark Meyer <mark.meyer@gsa.gov>
2024-03-07 13:57:11 -0900

    test out lastmodified to see if it works with pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 import process_anchors from "./src/plugins/process_anchors";
 import process_image_urls from './src/plugins/process_image_urls';
 import table_row_headers from './src/plugins/table_row_headers';
+import { remarkModifiedTime } from './src/plugins/remark-modified-time.mjs';
 
 const BASE_PATH = "/oasis-plus"
 
@@ -26,6 +27,7 @@ export default defineConfig({
   base: process.env.BASEURL,
   trailingSlash: 'always',
   markdown: {
+    remarkPlugins: [remarkModifiedTime],
     rehypePlugins: [
       [table_row_headers, {}],
       [process_anchors, {baseURL: process.env.BASEURL || '/'}],

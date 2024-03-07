94b1bcd3d9b23404a202698e846694caaf3999cd
Mark Meyer <mark.meyer@gsa.gov>
2024-03-07 14:12:50 -0900

    temporarily write logs on build to debug

--- src/plugins/remark-modified-time.mjs
+++ src/plugins/remark-modified-time.mjs
@@ -5,5 +5,6 @@ export function remarkModifiedTime() {
     const filepath = file.history[0];
     const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
     file.data.astro.frontmatter.lastModified = result.toString();
+    console.log("file:", filepath, "date:", result.toString())
   };
 }

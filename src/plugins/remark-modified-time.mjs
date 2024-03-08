49ccc0e7f99e896e06bf699f2658d3fdf12d508a
Mark Meyer <mark.meyer@gsa.gov>
2024-03-08 08:37:30 -0900

    allow manually setting last-modified in front matter for cases when the data is not directly in the markdown (like the naics codes).

--- src/plugins/remark-modified-time.mjs
+++ src/plugins/remark-modified-time.mjs
@@ -1,6 +1,9 @@
 /**
- * This looks at the last git commit of the markdown files to determin the
+ * This looks at the last git commit of the markdown files to determine the
  * modification date. 
+ * 
+ * Alternatively users may override this date by adding a last_modified key to front-matter
+ * with the date as a string.
  * 
  * When running on cloud.gov pages, it is important to tell cloud.gov to clone
  * the full repo — the default is a shallow clone. 
@@ -12,8 +15,15 @@ import { execSync } from "child_process";
 
 export function remarkModifiedTime() {
   return function (tree, file) {
-    const filepath = file.history[0];
-    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
-    file.data.astro.frontmatter.lastModified = result.toString();
+    // allow content creators to override date in front-matter
+    const manual_last_modified_date = file.data.astro.frontmatter.last_modified
+    if (manual_last_modified_date) {
+      file.data.astro.frontmatter.lastModified = manual_last_modified_date
+    }
+    else {
+      const filepath = file.history[0];
+      const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+      file.data.astro.frontmatter.lastModified = result.toString();
+    }
   };
 }

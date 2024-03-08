e7f6c60735b06e09111947adb6e60b1b8cd4c801
Mark Meyer <mark.meyer@gsa.gov>
2024-03-07 15:09:22 -0900

    remove debug loggin, add federalist.json to specify deep clones

--- src/plugins/remark-modified-time.mjs
+++ src/plugins/remark-modified-time.mjs
@@ -1,3 +1,13 @@
+/**
+ * This looks at the last git commit of the markdown files to determin the
+ * modification date. 
+ * 
+ * When running on cloud.gov pages, it is important to tell cloud.gov to clone
+ * the full repo — the default is a shallow clone. 
+ * 
+ * This is specified in the root-level federalist.json file
+ * See: https://cloud.gov/pages/documentation/federalist-json/
+ */
 import { execSync } from "child_process";
 
 export function remarkModifiedTime() {
@@ -5,6 +15,5 @@ export function remarkModifiedTime() {
     const filepath = file.history[0];
     const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
     file.data.astro.frontmatter.lastModified = result.toString();
-    console.log("file:", filepath, "date:", result.toString())
   };
 }
